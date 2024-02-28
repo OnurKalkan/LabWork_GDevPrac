1440144bf98fff2393a27c4c97f24b21dbe8fad2
OnurKalkan <onurkalkanv84@hotmail.com>
2024-02-28 14:38:56 +0300

    Hello World kodu oluşturuldu

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void Start()
+    {        
+        //Invoke(nameof(FrameThree), 3);
+        StartCoroutine(FrameThree());
+        print("Start World");
+    }
+
+    IEnumerator FrameThree()
+    {
+        yield return new WaitForSeconds(Time.deltaTime * 3);
+        print("Hello World");
+    }
+
+    private void Awake()
+    {
+        print("Awake World");
+    }
+}
